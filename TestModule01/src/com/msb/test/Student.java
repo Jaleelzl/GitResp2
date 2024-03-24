b194930818667790e5c62b3d645edb69bb5d2294
Jaleel <2488078048@qq.com>
2024-03-24 17:31:21 +0800

    删除注释，增加age属性和name属性

--- TestModule01/src/com/msb/test/Student.java
+++ TestModule01/src/com/msb/test/Student.java
@@ -1,10 +1,7 @@
 package com.msb.test;
 
-/**
- * @Auther: Jaleel Lei Zhou
- * @Date: 2024/3/24 - 03 - 24 - 17:25
- * @Description: com.msb.test
- * @version: 1.0
- */
+
 public class Student {
+    private int age;
+    private String name;
 }
