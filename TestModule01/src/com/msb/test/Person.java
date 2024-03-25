50446937bc9c5d93ad46862ca41f2cfb8347fa83
Jaleel <2488078048@qq.com>
2024-03-25 14:27:19 +0800

    给Person类增加私有属性height

--- TestModule01/src/com/msb/test/Person.java
+++ TestModule01/src/com/msb/test/Person.java
@@ -1,11 +1,8 @@
 package com.msb.test;
 
-/**
- * @Auther: Jaleel Lei Zhou
- * @Date: 2024/3/25 - 03 - 25 - 11:28
- * @Description: com.msb.test
- * @version: 1.0
- */
+
 public class Person {
     private int age;
+    private double height;
+
 }
